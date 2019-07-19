df7dae157c589575af11c958f59a374d2eff0b2c
Md. Morshadun Nur <morshadunnur@gmail.com>
2019-07-19 17:37:01 +0600

    sales & purchase angular

---
+++ public/js/sale.js
@@ -0,0 +1,81 @@
+(function(){
+    var app = angular.module('smgpos', [ ]);
+
+    app.controller("SearchItemCtrl", [ '$scope', '$http', function($scope, $http) {
+        $scope.items = [ ];
+        $http.get('api/item').success(function(data) {
+            $scope.items = data;
+        });
+        $scope.customers = [ ];
+        $http.get('api/customer').success(function(data) {
+            $scope.customers = data;
+        });
+        $scope.currentDue = [ ];
+        $scope.customerDue = function (id) {
+            $http.get('api/currentDue/' + id).success(function(data) {
+                $scope.currentDue = data;
+            });
+        };
+
+        $scope.message = [ ];
+        $scope.saletemp = [ ];
+        $scope.newsaletemp = { };
+        $http.get('api/saletemp').success(function(data, status, headers, config) {
+            $scope.saletemp = data;
+        });
+        $scope.addSaleTemp = function(item, newsaletemp) {
+            $http.post('api/saletemp', { item_id: item.id, cost_price: item.cost_price, selling_price: item.selling_price,discount:item.discount, quantity: item.quantity }).
+            success(function(data, status, headers, config) {
+                $scope.message.push(data);
+                $scope.saletemp.push(data);
+                $http.get('api/saletemp').success(function(data) {
+                    $scope.saletemp = data;
+                });
+            });
+        }
+        $scope.updateSaleTemp = function(newsaletemp) {
+
+            $http.put('api/saletemp/' + newsaletemp.id, {selling_price: newsaletemp.selling_price,discount:newsaletemp.discount, quantity: newsaletemp.quantity, total_cost: newsaletemp.item.cost_price * newsaletemp.quantity, total_selling: newsaletemp.selling_price * newsaletemp.quantity - newsaletemp.quantity * newsaletemp.discount }).
+            success(function(data, status, headers, config) {
+                // location.reload();
+            });
+        }
+        $scope.removeSaleTemp = function(id) {
+            $http.delete('api/saletemp/' + id).
+            success(function(data, status, headers, config) {
+                $http.get('api/saletemp').success(function(data) {
+                    $scope.saletemp = data;
+                });
+            });
+        }
+        $scope.sum = function(list) {
+            var total=0;
+            angular.forEach(list , function(newsaletemp){
+                total+= parseFloat(newsaletemp.selling_price * newsaletemp.quantity - (newsaletemp.quantity * newsaletemp.discount));
+            });
+            return total;
+        }
+
+        //specific word search
+        $scope.filterData = function (obj) {
+            return anyNameStartsWith(obj.item_name, $scope.searchKeyword);
+        };
+
+        function anyNameStartsWith (fullname, search) {
+
+            //validate if name is null or not a string if needed
+            if (search === '')
+                return true;
+
+            var delimeterRegex = /[ _-]+/;
+            //split the fullname into individual names
+            var names = fullname.split(delimeterRegex);
+
+            //do any of the names in the array start with the search string
+            return names.some(function(name) {
+                return name.toLowerCase().indexOf(search.toLowerCase()) === 0;
+            });
+        }
+
+    }]);
+})();
--- resources/views/admin/saleinvoice/index.blade.php
+++ resources/views/admin/saleinvoice/index.blade.php
@@ -1,1 +1,22 @@
-<?php
+
+@extends('template.app')
+@section('title', 'Sale Invoice')
+
+@push('css')
+    {!! Html::script('js/angular.min.js', array('type' => 'text/javascript')) !!}
+    {!! Html::script('js/sale.js', array('type' => 'text/javascript')) !!}
+
+@endpush
+
+@section('content')
+    <div class="page-bar">
+        <div class="page-title-breadcrumb">
+
+        </div>
+    </div>
+    <div class="row">
+
+    </div>
+
+
+@endsection
