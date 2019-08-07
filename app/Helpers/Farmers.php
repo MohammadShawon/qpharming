97f3e41de893c1e877fc859bd21c1ae5751ed2cf
Md. Morshadun Nur <morshadunnur@gmail.com>
2019-08-07 23:23:48 +0600

    Farmers Total Cost

--- app/Helpers/Farmers.php
+++ app/Helpers/Farmers.php
@@ -6,6 +6,7 @@ use App\Models\FarmerBatch;
 use App\Models\FarmerInvoice;
 use App\Models\Payment;
 use App\Models\ProductPrice;
+use App\Models\Farmer;
 
 class Farmers
 {
@@ -13,24 +14,29 @@ class Farmers
     {
         $chicksQuantity = FarmerBatch::where('farmer_id',$farmer_id)->where('status','active')->first();
 
-
-        $chicks = Category::where('name','Chicks')->with('products')->first();
-        $chicksPrice = [];
-        foreach ($chicks->products as $product)
+        if($chicksQuantity !== null)
         {
-
-            if (ProductPrice::where('product_id',$product->id)->where('batch_no',$chicksQuantity->chicks_batch_no)->first())
+            $chicks = Category::where('name','Chicks')->with('products')->first();
+            $chicksPrice = [];
+            foreach ($chicks->products as $product)
             {
-                $chicksPrice = ProductPrice::where('product_id',$product->id)->where('batch_no',$chicksQuantity->chicks_batch_no)->first();
+                if (ProductPrice::where('product_id',$product->id)->where('batch_no',$chicksQuantity->chicks_batch_no)->first())
+                {
+                    $chicksPrice = ProductPrice::where('product_id',$product->id)->where('batch_no',$chicksQuantity->chicks_batch_no)->first();
+                }
             }
 
+            $opening_balance = Farmer::where('id',$farmer_id)->sum('opening_balance');
+            $totalChicksPrice = $chicksPrice->selling_price * $chicksQuantity->chicks_quantity;
+            $totalInvoiceAmount = FarmerInvoice::where('farmer_id',$farmer_id)->sum('total_amount');
+            $advancePayments = Payment::where('farmer_id',$farmer_id)->sum('payment_amount');
+            return ($totalChicksPrice + $totalInvoiceAmount + $advancePayments + $opening_balance);
         }
-
-
-        $totalChicksPrice = $chicksPrice->selling_price * $chicksQuantity->chicks_quantity;
+        $opening_balance = Farmer::where('id',$farmer_id)->sum('opening_balance');
         $totalInvoiceAmount = FarmerInvoice::where('farmer_id',$farmer_id)->sum('total_amount');
         $advancePayments = Payment::where('farmer_id',$farmer_id)->sum('payment_amount');
+        return ($totalInvoiceAmount + $advancePayments + $opening_balance);
 
-        return ($totalChicksPrice + $totalInvoiceAmount + $advancePayments);
+
     }
 }
