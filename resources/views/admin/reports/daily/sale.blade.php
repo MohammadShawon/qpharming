fb2c52145680f6e44b222b7befb274deb1f6a6fa
shawon <shawon@Shantus-MacBook-Pro.local>
2019-11-04 11:00:06 +0600

    print template for pdf

--- resources/views/admin/reports/daily/sale.blade.php
+++ resources/views/admin/reports/daily/sale.blade.php
@@ -2,12 +2,17 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Daily Sales Report</title>
     <link rel="stylesheet" href="{{ asset('css/print.css') }}">
-
+    <style>
+        .page-break {
+            page-break-after: always;
+        }
+    </style>
 </head>
 <body>
     <header>
